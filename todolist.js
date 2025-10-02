31216e00f09ffe392539b7fc42582ef170464db7
Evelyne Munguti <evelynekyamwai@gmail.com>
2025-10-02 22:46:17 +0300

    Added a render todo function and also  a localstorage to make sure whatever task added does not get lost after reloading

--- todolist.js
+++ todolist.js
@@ -4,29 +4,47 @@ const dataEl = document.getElementById("data-el");
 const saveEl = document.getElementById("save-el");
 const listEl = document.querySelector(".list-el");
 
+let todos= JSON.parse(localStorage.getItem("todos")) ||[];
+
+function renderTodos() {
+    listEl.innerHTML = ""; 
+    todos.forEach(todo => {
+        listEl.innerHTML += `
+            <div style="
+                padding:7px;
+                margin: 5px 0;
+                border: none;
+                border-radius: 10px;
+                background-color: rgb(204, 226, 245);">
+                ${todo} 
+            </div>
+        `;
+    });
+}
+
+// Load existing todos on page load
+renderTodos();
+
+
 
 saveEl.addEventListener("click",function(){
     let todo=dataEl.value;
     
 
     if(todo===""){
-        alert("Please enter a ask");
+        alert("Please enter a task");
     }
    else{
-    listEl.innerHTML += `<div style ="
-    padding:7px;
-    margin: 5px 0;
-    border: none;
-    border-radius: 10px;
-    background-color: rgb(204, 226, 245);">
-    ${todo} 
-    </div>`;
-
-    dataEl.value="";
-   }
+    todos.push(todo); // add new todo
+    localStorage.setItem("todos", JSON.stringify(todos)); // save updated array
+    renderTodos(); // re-render list
+    dataEl.value = ""; 
     
-
+    
+   }
 })
+
+
     
 
 
